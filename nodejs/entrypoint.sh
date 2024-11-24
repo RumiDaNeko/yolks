63cddf8bb001f202aef224761e891b4dc636dd8b
Harumiii._. <ntncar311@gmail.com>
2024-11-24 14:06:52 +0700

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -4,18 +4,16 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
-apt install wget -y
-# Download Parser
-wget -O /etc/parsepackage.js https://raw.githubusercontent.com/NotMinhDucGamingTV/Cloudcodehosting-sourcenconfig/refs/heads/main/nodejs/parsepackage.js
 
 # Print Node.js Version
 node -v
 
-node /etc/parsepackage.js
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Run the Parser
+node /etc/parsepackage.js
 # Run the Server
 eval ${MODIFIED_STARTUP}
