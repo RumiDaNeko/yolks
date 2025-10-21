5724311daeddb903aef2d20669273de2317c3472
Harumiii._. <77107580+RumiDaNeko@users.noreply.github.com>
2025-10-21 13:16:55 +0700

    Create entrypoint.sh

---
+++ wings/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/container
+
+# Information output
+echo "Running on Debian $(cat /etc/debian_version)"
+echo "Current timezone: $(cat /etc/timezone)"
+wine --version
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
